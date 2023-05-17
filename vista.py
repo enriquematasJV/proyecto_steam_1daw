7eef37e2d1544bfde881370cea9edf52d8e988e5
Enrique <enriquematas@iesjulioverne.es>
2023-05-17 02:38:39 +0100

    cambios en el menu

--- vista.py
+++ vista.py
@@ -8,7 +8,7 @@ class Vista:
         self.sense=SenseHat()
         
     def menu(self):
-        print("0. Menú:")
+        print("0. Menú principal:")
         print("1. Medir temperatura")
         print("2. Medir presión")
         print("3. Medir humedad")
